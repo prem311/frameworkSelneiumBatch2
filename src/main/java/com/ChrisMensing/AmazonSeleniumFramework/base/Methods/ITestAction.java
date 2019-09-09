a52f40fadf7a4c0c6dc449ca7efc844ef48f36de
Christopher.Mensing <Christopher.Mensing@ASC1514-bl-pc.Vora-IT.net>
2019-09-09 13:31:54 -0400

    Removed A few Not needed Classes

--- src/main/java/com/ChrisMensing/AmazonSeleniumFramework/base/Methods/ITestAction.java
+++
@@ -1,8 +0,0 @@
-package com.ChrisMensing.AmazonSeleniumFramework.base.Methods;
-
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.interactions.Actions;
-
-public interface ITestAction {
-	Object Execute(WebElement WebElementToUse, Actions ActionToUse);
-}
